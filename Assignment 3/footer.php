d78aa6b528cb2967b43cfcacfaee7fd996e8407c
Aarsh Patel <114701316+Aarsh0811@users.noreply.github.com>
2022-12-04 15:22:50 -0500

    Update footer.php

--- Assignment 3/footer.php
+++ Assignment 3/footer.php
@@ -4,7 +4,10 @@ Student ID : 200520260
 Assignment#3 -->
 
 <!-- This is the code for our footer which can be called to our main pages when required -->
+<!DOCTYPE html>
+
 <footer class="bg-indigo rounded">
   <h2>END OF THE PAGE</h2>
 </footer>
+</html>
 <!-- End of the code for our footer -->
